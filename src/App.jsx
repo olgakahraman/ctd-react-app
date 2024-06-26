99d984d67c40b5f35263d78b2ea4c22120bc2b06
olgakahraman <ol.khrmn@gmail.com>
2024-06-26 10:53:54 -0400

    created AddTodoForm component and imported it into App.jsx file
    inside AddTodoForm component I created a form with label, input type "text" and submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 import './App.css';
+
 
 
 
@@ -8,7 +10,9 @@ function App() {
   return (
    <div>
     <h1>Todo List</h1>
+    <AddTodoForm />
     <TodoList />
+    
    </div>
   )
 }
