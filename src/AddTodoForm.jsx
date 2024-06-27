93e5ecc7ead7076875d7c1083c58dcd89ab8e6f7
olgakahraman <ol.khrmn@gmail.com>
2024-06-27 19:21:14 -0400

    Added a name attribute to the text input with value title

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function AddTodoForm(){
+  
 return(
   <div>
     <form>
-      <label for="todoTitle">Title</label>
-      <input type="text" id="todoTitle" />
+      <label for="todoTitle" >Title</label>
+      <input type="text" id="todoTitle" name="title"/>
       <button className="submit-btn">Add</button>
     </form>
   </div>
