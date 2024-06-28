90a105bf4a50f366266e9b72e20a9c196f1433ca
olgakahraman <ol.khrmn@gmail.com>
2024-06-27 20:02:24 -0400

    created handleAddTodo(event) function
    added prevent default to now renew page every time when submit. and added event.target.reset() method to reset the input value after submit in react app

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 export default function AddTodoForm(){
-  
+  function handleAddTodo(event){
+event.preventDefault();
+let todoTitle = document.querySelector("#todoTitle").value;
+console.log(todoTitle);
+event.target.reset();
+  }
 return(
   <div>
-    <form>
-      <label for="todoTitle" >Title</label>
+    <form onSubmit = {handleAddTodo}> 
+      <label htmlFor="todoTitle" >Title</label>
       <input type="text" id="todoTitle" name="title"/>
       <button className="submit-btn">Add</button>
     </form>
   </div>
 )
+
 }
