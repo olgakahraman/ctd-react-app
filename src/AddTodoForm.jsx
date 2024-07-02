5cd4a5cbdadbfb0726be2e49e77b51883cfa0c8a
olgakahraman <ol.khrmn@gmail.com>
2024-07-02 14:07:29 -0400

    added props as a parameter to AddTodoFunction and invoked callback prop and pass todoTitle as  an argument

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import { React } from 'react';
 
-export default function AddTodoForm(){
-  function handleAddTodo(event){
-event.preventDefault();
-let todoTitle = document.querySelector("#todoTitle").value;
-console.log(todoTitle);
-event.target.reset();
-  }
-return(
-  <div>
-    <form onSubmit = {handleAddTodo}> 
-      <label htmlFor="todoTitle" >Title</label>
-      <input type="text" id="todoTitle" name="title"/>
-      <button className="submit-btn">Add</button>
-    </form>
-  </div>
-)
+export default function AddTodoForm(props) {
+	function handleAddTodo(event) {
+		event.preventDefault();
+		let todoTitle =
+			document.querySelector('#todoTitle').value;
+		props.onAddTodo(todoTitle);
+		event.target.reset();
+	}
 
+	return (
+		<div>
+			<form onSubmit={handleAddTodo}>
+				<label htmlFor="todoTitle">Title</label>
+				<input type="text" id="todoTitle" name="title" />
+				<button className="submit-btn">Add</button>
+			</form>
+		</div>
+	);
 }
