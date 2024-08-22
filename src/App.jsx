2362b4b726eeb858bcd3642d587c97269059becd
olgakahraman <ol.khrmn@gmail.com>
2024-08-22 16:07:39 -0400

    added fetchData() with API key from secret folder

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,76 @@ import './App.css';
 
 function App() {
 	const [todoList, setTodoList] = useState([]);
-
 	const [isLoading, setIsLoading] = useState('true');
 
 	function addTodo(newTodo) {
 		setTodoList([...todoList, newTodo]);
 	}
 
-	useEffect(() => {
-		const p1 = new Promise((resolve, reject) => {
-			setTimeout(
-				() =>
-					resolve({
-						data: {
-							todoList:
-								JSON.parse(
-									localStorage.getItem('savedTodoList')
-								) || [],
-						},
-					}),
-				2000
-			);
-		}).then((result) => {
-			console.log(result);
-			setTodoList(result.data.todoList);
+	async function fetchData() {
+		const options = {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${
+					import.meta.env.VITE_AIRTABLE_API_TOKEN
+				}`,
+			},
+		};
+		const url = `https://api.airtable.com/v0/${
+			import.meta.env.VITE_AIRTABLE_BASE_ID
+		}/${import.meta.env.VITE_TABLE_NAME}`;
+		console.log('url ===> ', url);
+		try {
+			const response = await fetch(url, options);
+			if (!response.ok) {
+				const message = `Error has ocurred: ${response.status}`;
+				throw new Error(message);
+			}
+			const data = await response.json();
+			// console.log('data ===> ', data);
+
+			const todos = data.records.map((todo) => {
+				// const newTodo = {
+				// 	id: todo.id,
+				// 	title: todo.fields.title,
+				// };
+				// console.log();
+
+				// return newTodo;
+				return {
+					id: todo.id,
+					title: todo.fields.title,
+				};
+			});
+			console.log('todos ===> ', todos);
+
+			setTodoList(todos);
 			setIsLoading(false);
-		});
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+		// const p1 = new Promise((resolve, reject) => {
+		// 	setTimeout(
+		// 		() =>
+		// 			resolve({
+		// 				data: {
+		// 					todoList:
+		// 						JSON.parse(
+		// 							localStorage.getItem('savedTodoList')
+		// 						) || [],
+		// 				},
+		// 			}),
+		// 		2000
+		// 	);
+		// }).then((result) => {
+		// 	console.log(result);
+		// 	setTodoList(result.data.todoList);
+		// 	setIsLoading(false);
+		// });
 	}, []);
 
 	useEffect(() => {
