57bc9d78bac4fadcfd71fb5934558c065fdb412c
olgakahraman <ol.khrmn@gmail.com>
2024-08-22 21:04:41 -0400

    trying to make postTodo() work

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -8,9 +8,44 @@ export default function AddTodoForm({ onAddTodo }) {
 		let newTodoTitle = event.target.value;
 		setTodoTitle(newTodoTitle);
 	}
+	const postTodo =  async(newTodo) => {
+		try {
+			const airtableData = {
+				fields: {
+					title: newTodo,
+				},
+			};
+			const response = await fetch(
+				`https://api.airtable.com/v0/${
+					import.meta.env.VITE_AIRTABLE_BASE_ID
+				}/Default`,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${
+							import.meta.env.VITE_AIRTABLE_API_TOKEN
+						}`,
+					},
+					body: JSON.stringify(airtableData),
+				}
+			);
+			if (!response.ok) {
+				const message = `Error has ocurred: ${response.status}`;
+				throw new Error(message);
+			}
+			const dataResponse = await response.json();
+			return dataResponse;
+			
+		} catch (error) {
+			console.log(error.message);
+			
+		}
+	}
 	function handleAddTodo(event) {
 		event.preventDefault();
 		onAddTodo({ title: todoTitle, id: Date.now() });
+		postTodo();
 		setTodoTitle('');
 	}
 	return (
--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,40 @@ function App() {
 		// });
 	}, []);
 
+	// const postTodo =  async(newTodo) => {
+	// 	try {
+	// 		const airtableData = {
+	// 			fields: {
+	// 				title: newTodo,
+	// 			},
+	// 		};
+	// 		const response = await fetch(
+	// 			`https://api.airtable.com/v0/${
+	// 				import.meta.env.VITE_AIRTABLE_BASE_ID
+	// 			}/Default`,
+	// 			{
+	// 				method: 'POST',
+	// 				headers: {
+	// 					'Content-Type': 'application/json',
+	// 					Authorization: `Bearer ${
+	// 						import.meta.env.VITE_AIRTABLE_API_TOKEN
+	// 					}`,
+	// 				},
+	// 				body: JSON.stringify(airtableData),
+	// 			}
+	// 		);
+	// 		if (!response.ok) {
+	// 			const message = `Error has ocurred: ${response.status}`;
+	// 			throw new Error(message);
+	// 		}
+	// 		const dataResponse = await response.json();
+	// 		return dataResponse;
+			
+	// 	} catch (error) {
+	// 		console.log(error.message);
+			
+	// 	}
+	// }
 	useEffect(() => {
 		if (!isLoading)
 			localStorage.setItem(
