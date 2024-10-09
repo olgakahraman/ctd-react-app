65ae6dd32a8024fdd91c1eb21efe68d0d6eee5bd
olgakahraman <ol.khrmn@gmail.com>
2024-10-09 11:29:13 -0400

    added possible className which can let to change style in css module of element if it is clicked

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,33 +1,42 @@
 import PropTypes from 'prop-types';
-import React from 'react';
 import styles from './TodoListItem.module.css';
 
 export default function TodoListItem({
 	item,
+	onDoneTodo,
 	onRemoveTodo,
 }) {
 	return (
-		<>
-			<li className={styles.ListItem}>
-				{item.title}
-				<div className={styles.btns}>
-					<button className={styles.btn} title="Done">
-						<i className="fa-solid fa-check"></i>
-					</button>
-					<button
-						className={styles.btn}
-						onClick={() => onRemoveTodo(item.id)}
-						title="Remove"
-					>
-						<i className="fa-solid fa-trash-can"></i>
-					</button>
-				</div>
-			</li>
-		</>
+		<li className={styles.ListItem}>
+			{item.title}
+			<div className={styles.btns}>
+				<button
+					className={`${styles.btn} ${
+						item.isDone ? styles.done : ''
+					}`}
+					onClick={() => onDoneTodo(item.id)}
+					title="Done"
+				>
+					<i className="fa-solid fa-check"></i>
+				</button>
+				<button
+					className={styles.btn}
+					onClick={() => onRemoveTodo(item.id)}
+					title="Remove"
+				>
+					<i className="fa-solid fa-trash-can"></i>
+				</button>
+			</div>
+		</li>
 	);
 }
 
 TodoListItem.propTypes = {
-	item: PropTypes.object,
-	onRemoveTodo: PropTypes.func,
+	item: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		isDone: PropTypes.bool,
+	}),
+	onDoneTodo: PropTypes.func,
+	onRemoveTodo: PropTypes.func.isRequired,
 };
